fb312673139eafebba1598b5417e4cd300cd8b8e
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-05-23 11:29:41 +1000

    completed forms

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Dropdown from "./Dropdown"
+
+export default function Homepage () {
+
+    const [sourceLang, setSourceLang] = useState("english")
+    const [outputLang, setOutputLang] = useState("english")
+
+    return(
+        <div>
+            <form>
+                <Dropdown sourceLang={ sourceLang } setSourceLang= {setSourceLang}/>
+                <textarea name="source-text" cols="30" rows="10"></textarea>
+                <Dropdown outputLang={ outputLang } setOutputLang= {setOutputLang}/>
+                <textarea name="source-text" cols="30" rows="10"></textarea>
+
+            </form>
+        </div>
+    )
+}
